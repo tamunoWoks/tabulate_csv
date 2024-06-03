4badee76b545db69b7c43f3c7c159c4a3c642b11
tamunoWoks <tamunosakiwokoma86@gmail.com>
2024-06-02 18:17:59 -0600

    Check that command-line args == 2

--- tabulate.py
+++ tabulate.py
@@ -3,7 +3,11 @@ from sys import argv, exit
 from tabulate import tabulate
 
 def main():
-    ...
+    if len(argv) < 2:
+        exit('Too few command-line arguments')
+    elif len(argv) > 2:
+        exit('Too many command-line arguments')
+
 
 
 if __name__ == '__main__':
