9ac164c8d7a77e067a4bccea5fe691676be8e961
tamunoWoks <tamunosakiwokoma86@gmail.com>
2024-06-02 18:43:29 -0600

    Update main() to implement tabular()

--- tabular.py
+++ tabular.py
@@ -8,14 +8,19 @@ def main():
     elif len(argv) > 2:
         exit('Too many command-line arguments')
     else:
-        if not argv[1].endswith('csv'):
+        if not argv[1].lower().endswith('csv'):
             exit('Not a CSV file')
-
+        else:
+            try:
+                table = tabular(argv[1])
+                print(table)
+            except FileNotFoundError:
+                exit('File does not exist')
 
 def tabular(file):
     with open(file) as f:
         reader = csv.reader(f)
-        table = tabulate(reader, header='firstrow', tablefmt='grid')
+        table = tabulate(reader, headers='firstrow', tablefmt='grid')
         return table 
 
 
